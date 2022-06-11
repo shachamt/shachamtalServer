244fb1a4e391f610e9e86f17130507c3ae78346e
shachamt <shachamt@post.bgu.ac.il>
2022-06-11 22:47:58 +0300

    exercise3

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, redirect
 from flask import url_for
+from datetime import datetime
 from flask import render_template
+from flask import request
 
 app = Flask (__name__)
 
 @app.route('/')
 def goHome ():
-    return redirect('/home')
+    return redirect(url_for('homePage'))
 
 @app.route('/home')
 def homePage ():
@@ -26,12 +28,33 @@ def MyHobbies ():
                            user_info=user_info,
                            my_shows=shows)
 
+users_dict =  {
+    'Shacham': 'shachamt@post.bgu.ac.il',
+    'Aviv': 'aviv@post.bgu.ac.il',
+    'Noy': 'noy@post.bgu.ac.il',
+    'Oron': 'Oron@post.bgu.ac.il',
+    'Shany': 'shany@post.bgu.ac.il',
 
-    return render_template('assignment3_1.html')
+}
 
 @app.route('/forms')
 def MyForms ():
-    return render_template('assignment3_2.html')
+    if 'user_name' in request.args:
+        user_name= request.args['user_name']
+        if user_name in users_dict:
+            return render_template('assignment3_2.html',
+                                   user_name=user_name,
+                                   user_email= users_dict[user_name])
+        else:
+            if user_name=='':
+                return render_template('assignment3_2.html',
+                                       isEmpty=True,
+                                       users_dict=users_dict)
+            else:
+                return render_template('assignment3_2.html',
+                                   message= 'user not found')
+    return render_template('assignment3_2.html',
+                           users_dict=users_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
